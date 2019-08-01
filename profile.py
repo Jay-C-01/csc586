cb71de9bca041103ded1e0a31e63dcc192c40ada
Jay-C-01 <53063598+Jay-C-01@users.noreply.github.com>
2019-08-01 14:15:03 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -30,8 +30,8 @@ for i in range(2):
   link.addInterface(iface)
   
    if i == 0:
-    node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/install_slapd.sh"))
-    node.addService(pg.Execute(shell="sh", command="/local/repository/install_slapd.sh"))
+    node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/install_server.sh"))
+    node.addService(pg.Execute(shell="sh", command="/local/repository/install_server.sh"))
   else:
     node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/install_client.sh"))
     node.addService(pg.Execute(shell="sh", command="/local/repository/install_client.sh"))
